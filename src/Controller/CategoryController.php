0e62f670d20d32f09d07beb78a0442a39bb63652
thuyannhu <nhu.thuy-an@hotmail.fr>
2023-05-24 11:58:45 +0200

    displayed programs based on category

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,47 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
+
+#[Route('/category/', name: 'category_')]
+class CategoryController extends AbstractController
+{
+    #[Route('', name: 'index')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+         ]);
+    }
+
+    #[Route('{categoryName}', methods: ['GET'], name: 'show')]
+
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
+    {
+        $category = $categoryRepository->findOneBy(['name' => $categoryName]);
+
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No program with category : '.$categoryName.' found in program\'s table.'
+            );
+        } else {
+        
+            $programs = $programRepository->findBy(
+                ['category' => $category->getId()],
+                ['id' => 'DESC'],
+                3
+            );
+        }
+
+        return $this->render('category/show.html.twig', [
+            'programs' => $programs,
+            'category' => $category,
+        ]);
+    }
+}
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,23 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProgramRepository;
 
 #[Route('/program/', name: 'program_')]
 class ProgramController extends AbstractController
 {
     #[Route('', name: 'index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
+            'programs' => $programs,
          ]);
     }
 
     #[Route('{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'show')]
     public function show(int $id = 1): Response
     {
+        $program = $programRepository->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+
         return $this->render('program/show.html.twig', [
-            'id' => $id,
+            'program' => $program,
         ]);
     }
 }
